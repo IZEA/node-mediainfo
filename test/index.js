8541d70130780f42b2efc255bf5e3747b1de7ae0
Kananek T <info.dvgamer@gmail.com>
2018-11-22 19:19:27 +0700

    update test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const mediainfo = require('../index.js')
 const util = require('util')
 
-mediainfo('F:/Google Drive/NAS-SERVER/Anime-Series/Anime Naruto Shippuuden/Naruto Shippuuden • Season 10 การประชุมของห้าคาเงะ/Naruto Shippuuden 215 [720p].mkv').then(data => {
+mediainfo('/file.mp4').then(data => {
   console.log(util.inspect(data, { showHidden: false }, null, true))
 }).catch(ex => {
   console.error(ex)
